1119b891558c19b63b8db2b18f7b886e2e4bb878
unknown <stmachin@gmail.com>
2023-08-06 10:21:41 -0500

    Initial testing

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,52 @@
 // Assignment Code
-var generateBtn = document.querySelector["#generate"];
-var passwordLength = [8<=128];
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numbers =['1','2','3','4','5','6','7','8','9','0'];
-var specialCharacters = ['!','@','#','$','%','^','&','*','(',')',];
+var generateBtn = document.querySelector("#generate");
+var passwordLength = 8;
+var choiceArr = [];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')',];
 
-function generatePassword(){
-  console.log("Start your Password!")
-  // 1. Prompt the user for the password criteria
-  //    a. pasword length 8 < 128
-  //    b. Lowercase, uppercase, numbers, special characters
- 
-  //2. Validate the Input
-  //3. Display Password
-  return "Generated password will go here!"
-}
-// Write password to the #password input
-function writePassword() {
- 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function genratePassword(){
+
+function generatePassword() {
+  console.log("Start your Password!")
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomLetter = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomLetter];
+  }
+}
+// Write password to the #password input
+function writePassword() {}
+  var correctPrompts = prompts();
+  if (correctPrompts) {
+    var newPassword = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = newPassword;
 
 }
-function getPrompts(){
-  passwordLength = prompt("How long do you want your password to be?")
+
+
+function prompts() {
+  passwordLength = parseInt(prompt("How long would you like your password to be?"));
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128)
+    return false;
+
+  if (confirm("Include lowercase letters?")) {
+    choiceArr = choiceArr.concat(lowerCase);
+  }
+  if (confirm("Include uppercase letters?")) {
+    choiceArr = choiceArr.concat(upperCase);
+  }
+  if (confirm("Include Numbers?")) {
+    choiceArr = choiceArr.concat(numbers);
+  }
+  if (confirm("Include special characters?")) {
+    choiceArr = choiceArr.concat(specialCharacters);
+  }
+  return true;
 }
